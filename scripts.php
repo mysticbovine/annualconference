c08d471dc9cee2ceb2ff0bdab25d32bf3cb580a9
Rob Evans <rob@rockycoastcreative.ca>
2019-02-07 18:17:15 -0800

    Added Google Analytics

--- scripts.php
+++ scripts.php
@@ -20,3 +20,13 @@
   
 	<!-- Custom JS -->
 	<script src="js/d96conference.js"></script>
+  
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-134154363-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-134154363-1');
+  </script>
