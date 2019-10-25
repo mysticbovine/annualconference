a50bcc6ea1734cb1ac59f60db3ccdeaa8d53608c
Rob Evans <rob@rockycoastcreative.ca>
2019-10-25 13:34:03 -0700

    Adding Communication Leadership pages and form.

--- banner-internal.php
+++ banner-internal.php
@@ -1,1 +1,1 @@
-<img src="images/96banner.jpg" alt="A woman is jumping in the air and positioned like she is running.  A mountain valley in the background." class="img-responsive center-block">
+<img src="images/96banner.jpg" alt="Retro styled moon and rocket ship on the sides. In the middle words say 'Shoot the Moon: Your Time to Shine'." class="img-responsive center-block">
