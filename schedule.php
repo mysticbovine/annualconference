faef905b7bd0b1638ed3251de78b11bf7873fae0
Rob Evans <rob@rockycoastcreative.ca>
2020-03-01 13:09:48 -0800

    Final bios added.

--- schedule.php
+++ schedule.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Annual Conference 2019 | Schedule | District 96 Toastmasters</title>
 
-	<meta name="author" content="Rob Evans DTM, Rocky Coast Creative, www.rockycoastcreative.ca">
+    <meta name="author" content="Rob Evans DTM, Rocky Coast Creative, www.rockycoastcreative.ca">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/d96conference.css" rel="stylesheet">
 
-	<!-- Custom Fonts -->
-	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700|Roboto+Slab:400,300,700' rel='stylesheet' type='text/css'>
+    <!-- Custom Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700|Roboto+Slab:400,300,700' rel='stylesheet'
+        type='text/css'>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -30,219 +32,253 @@
     <link rel="apple-touch-icon" sizes="144x144" href="/images/favicons/apple-icon-144x144.png">
     <link rel="apple-touch-icon" sizes="152x152" href="/images/favicons/apple-icon-152x152.png">
     <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="/images/favicons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="/images/favicons/android-icon-192x192.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="96x96" href="/images/favicons/favicon-96x96.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png">
     <link rel="manifest" href="/images/favicons/manifest.json">
     <meta name="msapplication-TileImage" content="/images/favicons/ms-icon-144x144.png">
-  </head>
-  <body class=" bg-yellow faded">
+</head>
+
+<body class=" bg-yellow faded">
     <?php include("nav.php");?>
-	<?php include("banner-internal.php");?>
-	<!-- Body Start -->
-	<div class="container body schedule">
-		<div class="col-sm-12 text-center">
-			<h1 class="page-header">Conference Schedule</h1>
-			
-		
-		</div>
-    
-    
-    <div class="row">
-      <div class="col-lg-12 text-center">
-        <p class="lead"><strong>Note: Schedule is subject to change. Please check this page or <a href="https://www.facebook.com/groups/District96/" target=_blank>BC Toastmasters Facebook Page</a> for updates</strong></p>
-        
-        <p> Page last updated: Feb 21, 2020</p>
-      </div>
-     
+    <?php include("banner-internal.php");?>
+    <!-- Body Start -->
+    <div class="container body schedule">
+        <div class="col-sm-12 text-center">
+            <h1 class="page-header">Conference Schedule</h1>
+
+
+        </div>
+
+
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <p class="lead"><strong>Note: Schedule is subject to change. Please check this page or <a
+                            href="https://www.facebook.com/groups/District96/" target=_blank>BC Toastmasters Facebook
+                            Page</a> for updates</strong></p>
+
+                <p> Page last updated: March 1, 2020</p>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th colspan="2">April 17. Friday Evening</th>
+
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td width="100"><strong> 4:00 pm</strong></td>
+                            <td> Registration Open</td>
+
+                        </tr>
+
+                        <tr>
+                            <td><strong>4:45 pm</strong></td>
+                            <td> First Timers’ Reception<br /><small>Hosted by Doug Anderson DTM</small></td>
+
+                        </tr>
+
+                        <tr>
+                            <td><strong>6:00 pm</strong></td>
+                            <td> Opening Session with Club Banner Parade</td>
+
+                        </tr>
+
+                        <tr>
+                            <td><strong>7:00 pm</strong></td>
+                            <td> Friday Fun Night Dinner, Dance and Speeches<br />
+                                <small>Visit the <a href="fridayfun.php">Friday Fun Night Presenter Page</a> for more
+                                    information<br /></small>
+
+                            </td>
+                        </tr>
+
+                        <td><strong>10:00 pm</strong></td>
+                        <td> 50/50 Draw and Silent Wine Auction</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th colspan="2">April 18. Saturday Morning</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td width="100px"><strong>7:00 am</strong></td>
+                            <td> Contestants and Judges Briefings</td>
+                        </tr>
+
+
+                        <tr>
+                            <td><strong>7:45 am</strong></td>
+                            <td> Breakfast and Candidates Showcase</td>
+                        </tr>
+
+
+                        <tr>
+                            <td><strong>9:30 am</strong></td>
+                            <td> Saturday Morning Education Sessions<br />
+                            
+                        
+                            <small >
+                              <br /><b>Personal Development Track:</b> Create Opportunities within Challenges in 3 Easy
+                              Steps by Adrian Starks<br />
+                              <b>Leadership Track:</b> How are you Smart? by Mariah-Jane Thies<br />
+                              <b>Communication Track:</b> How To Grow Your Online Visibility With Rapid Fire
+                              Videos by Moreno Perazollo<br /><br />
+                              Visit <a href="educations.php">Education Sessions</a> page for more
+                              information</a></small>
+                              
+                         
+                            
+                                
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><strong>10:30 am</strong></td>
+                            <td> Saturday Morning Education Sessions<br />
+                                <small><br />
+                                    <b>Personal Development Track:</b> Secrets to Remembering Names and Faces (and
+                                    Speeches too!) by Stefano Cossalter<br />
+                                    <b>Leadership Track:</b> Blind spots - Shooting for the moon and actually getting
+                                    there by Douglas McKegney<br />
+                                    <b>Communication Track:</b> Leaders – Your Mind is a Gold Mine bt Quinn
+                                    Cashion<br /><br />
+                                    Visit <a href="educations.php">Education Sessions</a> page for more
+                                    information</a></small>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><strong>11:30 am</strong></td>
+                            <td> Saturday Morning Education Sessions<br />
+                                <small><br />
+                                    <b>Personal Development Track:</b> Productivity- The Happy Secret to A Better Life Or The Happiness Advantage by Devon Downie<br />
+                                    <b>Leadership Track:</b> R.I.S.E. And Shoot For the Moon: Four Pillars of Success by Anita Adams<br />
+                                    <b>Communication Track:</b> How to Create a Resilient Dynamic Workplace Culture by Emi Garzitto<br /><br />
+                                    Visit <a href="educations.php">Education Sessions</a> page for more
+                                    information</a></small>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <thead>
+                        <tr>
+                            <th colspan="2">Afternoon</th>
+
+                        </tr>
+                    </thead>
+                    <tr>
+                        <td><strong>12:30 pm </strong></td>
+                        <td> Communication & Leadership Award Lunch</td>
+                    </tr>
+
+
+                    <tr>
+                        <td><strong>2:00 pm</strong></td>
+                        <td> District Council Business Meeting<br />
+                            <small>Visit <a href="businessmeeting.php">Business Meeting Page</a> for more
+                                information</a></small>
+                        </td>
+                    </tr>
+                    <tbody>
+                    </tbody>
+
+
+                    <thead>
+                        <tr>
+                            <th colspan="2">Evening</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td width="100px"><strong>4:30 pm</strong></td>
+                            <td> District 96 International Speech Contest</td>
+                        </tr>
+                        <tr>
+                            <td><strong>7:00 pm</strong></td>
+                            <td> Conference Dinner and Keynote<br />
+                                <small>Visit <a href="keynotes.php">Keynote Presenter</a> page for more
+                                    information</small>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><strong>9:00 pm</strong></td>
+                            <td> Silent and Live Auctions, 50/50 Draw</td>
+                        </tr>
+
+                        </tr>
+                    </tbody>
+                </table>
+
+
+
+
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th colspan="2">April 19. Sunday Morning</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td width="100px"><strong>7:00 am</strong></td>
+                            <td> Contestant and Official Briefings</td>
+                        </tr>
+                        <tr>
+                            <td><strong>8:00 am</strong></td>
+                            <td> Breakfast</td>
+                        </tr>
+                        <tr>
+                            <td><strong>8:30 am</strong></td>
+                            <td> Keynote Workshop</td>
+                        </tr>
+                        <tr>
+                            <td><strong>10:300 am</strong></td>
+                            <td> Yoga Moment</td>
+                        </tr>
+                        <tr>
+                            <td><strong>10:15 am</strong></td>
+                            <td> District 96 Evaluation Contest</td>
+                        </tr>
+                        <tr>
+                            <td><strong>11:30 am</strong></td>
+                            <td> Live Mentor Auctions</td>
+                        </tr>
+                        <tr>
+                            <td><strong>12:00 am</strong></td>
+                            <td> Gavel Club Presentations</td>
+                        </tr>
+                        <tr>
+                            <td><strong>12:15pm</strong></td>
+                            <td> 50/505 Draw and Closing ceremonies</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+
+
+
+        </div>
     </div>
-
-    <div class="row">
-     
-      <div class="col-lg-12">
-        <table class="table">
-          <thead>
-            <tr>
-              <th colspan="2">April 17. Friday Evening</th>
-             
-            </tr>
-          </thead>
-
-          <tbody>	
-            <tr>
-              <td width="100px"><strong> 4:00 pm</strong></td>
-              <td> Registration Open</td>
-              
-            </tr>
-            
-            <tr>					
-              <td><strong>4:45 pm</strong></td>
-              <td> First Timers’ Reception<br /><small>Hosted by Doug Anderson DTM</small></td>
-              
-            </tr>
-
-            <tr>
-              <td><strong>6:00 pm</strong></td>
-              <td> Opening Session with Club Banner Parade</td>
-              
-            </tr>
-
-            <tr>
-              <td><strong>7:00 pm</strong></td>
-              <td> Friday Fun Night Dinner, Dance and Speeches<br />
-              <small>Visit the <a href="fridayfun.php">Friday Fun Night Presenter Page</a> for more information<br /></small>
-              <!-- <b>Winston Yeung</b> – <i>Why Are You Only Aiming for the Moon?</i><br />
-              <b>Mai Paredes</b> – <i>Flamenco <br />
-              <b>Tom Everitt</b> – <i>Around the World in 80 Anthems: A Story of Inspiration</i><br />
-              <b>Arthur Gee</b> – <i>With Mai and Kyriaki</i><br />
-              <b>Michelle Tremblay</b> – <i>MPower  Your Inner Ninja</i><br />
-              <b>Kyriaki Papadopoulou</b> – <i>Belly Dancing</i> -->
-              </td>
-            </tr>  
-           
-              <td><strong>10:00 pm</strong></td>
-              <td> 50/50 Draw and Silent Wine Auction</td>
-            </tr>            
-          </tbody>
-        </table>
-      </div>
-    </div>
-  <div class="row">
-    <div class="col-lg-12">
-   
-      <table class="table table-hover">
-        <thead>
-          <tr>
-            <th colspan="2">April 18. Saturday Morning</th>
-  
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td width="100px"><strong>7:00 am</strong></td>
-            <td> Contestants and Judges Briefings</td>
-          </tr>
-
-
-          <tr>
-            <td><strong>7:45 am</strong></td>
-            <td> Breakfast and Candidates Showcase</td>
-          </tr>
-
-
-          <tr>
-            <td><strong>9:30 am</strong></td>
-            <td> Saturday Morning 9 Education Sessions<br />
-                <small>Visit <a href="educations.php">Education Sessions</a> page for more information</a></small>
-            </td>
-          </tr>
-        </tbody>
-        <thead>
-          <tr>
-            <th colspan="2">Afternoon</th>
-      
-          </tr>
-        </thead>
-          <tr>
-            <td><strong>12:30 pm </strong></td>
-            <td> Communication & Leadership Award Lunch</td>
-          </tr>
-
-
-          <tr>
-            <td><strong>2:00 pm</strong></td>
-            <td> District Council Business Meeting<br />
-            <small>Visit <a href="businessmeeting.php">Business Meeting Page</a> for more information</a></small>
-            </td>
-          </tr>
-         <tbody>
-         </tbody>
-
-  
-          <thead>
-            <tr>
-              <th colspan="2">Evening</th>
-            
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td width="100px"><strong>4:30 pm</strong></td>
-              <td> District 96 International Speech Contest</td>
-            </tr>
-            <tr>
-              <td><strong>7:00 pm</strong></td>
-              <td> Conference Dinner and Keynote<br />
-                <small>Visit <a href="keynotes.php">Keynote Presenter</a> page for more information</small>
-              </td>
-            </tr>
-            <tr>
-              <td><strong>9:00 pm</strong></td>
-              <td> Silent and Live Auctions, 50/50 Draw</td>
-            </tr>
-            
-            </tr>
-          </tbody>
-        </table>
- 
-     
-
-     
-      <table class="table table-hover">
-        <thead>
-          <tr>
-            <th colspan="2">April 19. Sunday Morning</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td width="100px"><strong>7:00 am</strong></td>
-            <td> Contestant and Official Briefings</td>
-          </tr>
-          <tr>
-            <td><strong>8:00 am</strong></td>
-            <td> Breakfast</td>
-          </tr>
-          <tr>
-            <td><strong>8:30 am</strong></td>
-            <td> Keynote Workshop</td>
-          </tr>
-          <tr>
-            <td><strong>10:300 am</strong></td>
-            <td> Yoga Moment</td>
-          </tr>
-          <tr>
-            <td><strong>10:15 am</strong></td>
-            <td> District 96 Evaluation Contest</td>
-          </tr>
-          <tr>
-            <td><strong>11:30 am</strong></td>
-            <td> Live Mentor Auctions</td>
-          </tr>
-          <tr>
-            <td><strong>12:00 am</strong></td>
-            <td> Gavel Club Presentations</td>
-          </tr>
-          <tr>
-            <td><strong>12:15pm</strong></td>
-            <td> 50/505 Draw and Closing ceremonies</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-
-
-
-
-		</div>
-	</div>
-	<!-- Body End -->
-	<!-- Footer Start -->
-	<!-- Contact Section -->
+    <!-- Body End -->
+    <!-- Footer Start -->
+    <!-- Contact Section -->
     <?php include("contact.php");?>
-	<!-- Footer End -->
+    <!-- Footer End -->
 
     <?php include("scripts.php");?>
-  </body>
+</body>
+
 </html>
