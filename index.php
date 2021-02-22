b227f6d004c50f4a6eefeb1a45b9d23ec40bd123
Kimberly Huynh <64296901+huynhkimberly@users.noreply.github.com>
2021-02-21 20:47:29 -0700

    Update index.php
    Removed Kim learning test comment.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Kim is learning Git. The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Annual Conference 2021 | District 96 Toastmasters</title>
 
 	<meta name="author" content="Rob Evans DTM, Rocky Coast Creative, www.rockycoastcreative.ca">
